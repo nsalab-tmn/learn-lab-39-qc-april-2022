f1543c4e0ffc46a567dbc29682d86846eb28fb94
Aleksandr Gorbachev <menus12@mail.ru>
2022-04-08 15:36:23 +0000

    fixed user provisioner

--- deploy/user_deprovisioner.sh
+++ deploy/user_deprovisioner.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Unset project from user
-curl -w -L -X PUT "https://$(cat /tmp/user_data.out | jq -r .domain.name)/v1/client/$(cat /tmp/user_data.out | jq -r .entities[].entity.id)/team/$(cat /tmp/user_data.out | jq -r .entities[].member_id)" -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer $bearer" -o /tmp/user_unset.out >/dev/null 2>&1 --data-raw '{
+curl -w -L -X PUT "https://$(cat /tmp/user_data.out | jq -r .domain.name)/v1/client/$(cat /tmp/user_data.out | jq -r .entities[].entity.id)/team/$(cat /tmp/user_data.out | jq -r .entities[].member_id)" -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer $(head -c -1 /tmp/bearer)" -o /tmp/user_unset.out >/dev/null 2>&1 --data-raw '{
   "user": "'"$(cat /tmp/user_data.out | jq -r .id)"'",
   "role": "'"$(cat /tmp/user_data.out | jq -r .entities[].role.id)"'",
   "acl_list": [ ]
