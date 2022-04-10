c77a8605ede9a97ab8bba417b32692be82aa6d26
Aleksandr Gorbachev <menus12@mail.ru>
2022-04-10 11:31:35 +0000

    fix curl options

--- deploy/web_service_kickstart.sh
+++ deploy/web_service_kickstart.sh
@@ -4,10 +4,10 @@
 set -e
 
 # Login as admin
-curl -m 10 -b /tmp/cookie -c /tmp/cookie -X POST -d '{"username":"'"$login"'","password":"'"$old_password"'"}' $endpoint/api/auth/login -o /tmp/web_login.out >/dev/null 2>&1
+curl -w -k -L -b /tmp/cookie -c /tmp/cookie -X POST -d '{"username":"'"$login"'","password":"'"$old_password"'"}' $endpoint/api/auth/login -o /tmp/web_login.out >/dev/null 2>&1
 
 # Change admin password
-curl -m 10 -c /tmp/cookie -b /tmp/cookie -X PUT -d '{"name":"'"$login"'","email":"root@localhost","password":"'"$new_password"'","role":"admin","expiration":"-1","pod":0,"pexpiration":"-1"}' -H 'Content-type: application/json' $endpoint/api/users/admin -o /tmp/web_passwd.out >/dev/null 2>&1
+curl -w -k -L -c /tmp/cookie -b /tmp/cookie -X PUT -d '{"name":"'"$login"'","email":"root@localhost","password":"'"$new_password"'","role":"admin","expiration":"-1","pod":0,"pexpiration":"-1"}' -H 'Content-type: application/json' $endpoint/api/users/admin -o /tmp/web_passwd.out >/dev/null 2>&1
 
 # Login
 curl -w -k -L -b /tmp/cookie -c /tmp/cookie -X POST -d '{"username":"'"$login"'","password":"'"$new_password"'"}' $endpoint/api/auth/login  -o /tmp/web_login.out >/dev/null 2>&1
