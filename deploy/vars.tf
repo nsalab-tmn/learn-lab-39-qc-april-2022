6862ff586f0f76f13fb8723f79ab556e7e808399
menus12 <menus12@mail.ru>
2023-10-04 14:35:26 -0700

    fix

--- deploy/vars.tf
+++ deploy/vars.tf
@@ -2,9 +2,5 @@ variable "rustack_admin_token" {
     type        = string
 
     # Self client admin
-    default     = "054be40e142bede48c0721c36f9e88c327107a2c"
-
-    # Guest client admin
-    # default     = "591cac98d205ceaae742d236ecce4a1bd614acba"
-    
+    default     = "api_key"
 }
